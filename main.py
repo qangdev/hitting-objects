36f16e5b3be405791c6b8fee6ed66dd0ad766295
qangdev <qangdev@gmail.com>
2021-05-14 08:07:08 +0700

    Updated: Fixbug slope calculating wrong

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ class Point:
         self.taken = False
 
     def __repr__(self):
-        return f"x:{self.x}|y:{self.y}"
+        return f"({self.x}:{self.y}"
+
+    def __str__(self):
+        return f"{self.x}:{self.y}"
 
     def slope(self, other):
         if other.x == self.x:
             return 0
         return (other.y - self.y) / (other.x - self.x)
-
-    def is_collinear(self, other):
-        return self.slope(other) == 1
 
     def mark_taken(self):
         self.taken = True
@@ -25,82 +25,91 @@ class Point:
     def is_taken(self):
         return self.taken
 
+
+class Line:
+    def __init__(self, points: List[Point]):
+        self.points = points
+        self.taken = False
+
+    def __add__(self, other):
+        self.points + other.points
+
+    def __repr__(self):
+        return "|".join([str(p) for p in self.points])
+
+    def get_points(self):
+        return self.points
+
+    def unpack_points(self):
+        return self.points
+
+    def mark_taken(self):
+        self.taken = True
+
+    def is_taken(self):
+        return self.taken
+
+
 class Manager:
     def __init__(self, points: List[list]):
-        self.comb_points: list = points
-        self.lines: list = []
-        self.count: int = 0
+        self.points: list = []
+        self.pair_points: list = self.generate_pair_points(points)
+        self.lines: List[Line] = []
 
-    def collinear(self, p1: Point, p2: Point, p3: Point) -> bool:
+    def generate_pair_points(self, points) -> list:
+        return list(itertools.combinations(points, 3))
+
+    def generate_pair_lines(self, lines) -> list:
+        return list(itertools.combinations(lines, 2))
+
+    def collinear_points(self, p1: Point, p2: Point, p3: Point) -> bool:
         is_p1p2_collinear = p1.slope(p2)
         is_p2p3_collinear = p2.slope(p3)
-        return is_p1p2_collinear == is_p2p3_collinear
+        is_p1p3_collinear = p1.slope(p3)
+        return is_p1p2_collinear == is_p2p3_collinear == is_p1p3_collinear
 
+    def collinear_lines(self, l1: Line, l2: Line):
+        l1p1, l1p2, l1p3 = l1.unpack_points()
+        l2p1, l2p2, l2p3 = l2.unpack_points()
+
+        l1p1_l1p2_slope = l1p1.slope(l1p2)
+        l1p2_l1p3_slope = l1p2.slope(l1p3)
+        l1p3_l2p1_slope = l1p3.slope(l2p1)
+        l2p1_l2p2_slope = l2p1.slope(l2p2)
+        l2p2_l2p3_slope = l2p2.slope(l2p3)
+        l1p1_l2p3_slope = l1p1.slope(l2p3)
+
+        return l1p1_l1p2_slope == l1p2_l1p3_slope == l1p3_l2p1_slope == l2p1_l2p2_slope == l2p2_l2p3_slope == l1p1_l2p3_slope
+
+    # TODO: Refactor this function
     def process(self):  # need a better name for this
-        for each_pair in self.comb_points:
+        for each_pair in self.pair_points:
             p1, p2, p3 = each_pair
             if p1.is_taken() or p2.is_taken() or p3.is_taken():
                 continue
-            if self.collinear(p1, p2, p3):
+            if self.collinear_points(p1, p2, p3):
                 p1.mark_taken()
                 p2.mark_taken()
                 p3.mark_taken()
-                self.lines.append(each_pair)
-                self.count += 1
+                self.lines.append(Line(each_pair))
+        if self.lines:
+            for pair_line in self.generate_pair_lines(self.lines):
+                line_one, line_two = pair_line
+                if line_one.is_taken() or line_two.is_taken():
+                    continue
+                if self.collinear_lines(line_one, line_two):
+                    line_one.mark_taken()
+                    line_two.mark_taken()
+                    self.merge_lines(line_one, line_two)
 
-    def get_count(self):
-        return self.count
+    def merge_lines(self, line_1, line_2):
+        new_line = Line(points=line_1.get_points() + line_2.get_points())
+        self.lines.remove(line_1)
+        self.lines.remove(line_2)
+        self.lines.append(new_line)
+
+    def count_line(self):
+        return len(self.lines)
+
     def get_lines(self):
         return self.lines
-
-
-if __name__ == '__main__':
-    # points = [Point(1, 1),
-    #           Point(1, 2),
-    #           Point(1, 3)]
-
-    # points = [Point(1, 1),
-    #           Point(2, 2),
-    #           Point(3, 3),
-    #           Point(4, 4)
-    #           Point(5, 5)
-    #           Point(6, 6)]
-
-    # points = [Point(10, 1),
-    #           Point(10, 2),
-    #           Point(10, 3),
-    #           Point(11, 4),
-    #           Point(11, 5),
-    #           Point(11, 6)]
-
-    # points = [Point(7, 1),
-    #           Point(7, 2),
-    #           Point(8, 3),
-    #           Point(8, 4),
-    #           Point(9, 5),
-    #           Point(9, 6)]
-
-    points = [Point(1, 1),
-              Point(2, 2),
-              Point(3, 3),
-              Point(4, 4),
-              Point(5, 5),
-              Point(6, 6),
-              Point(7, 1),
-              Point(7, 2),
-              Point(8, 3),
-              Point(8, 4),
-              Point(9, 5),
-              Point(9, 6),
-              Point(10, 1),
-              Point(10, 2),
-              Point(10, 3),
-              Point(11, 4),
-              Point(11, 5),
-              Point(11, 6)]
-
-    comb_points = list(itertools.combinations(points, 3))
-    manager = Manager(list(comb_points))
-    manager.process()
-
-    assert 1 == manager.get_count()
--- test_manager.py
+++ test_manager.py
@@ -1,4 +1,15 @@
+import pytest
 from main import Manager, Point
+
+
+def test_simple_line():
+    points = [Point(1, 1),
+              Point(2, 2),
+              Point(3, 3)]
+    manager = Manager(points)
+    manager.process()
+    assert 1 == manager.count_line()
+
 
 def test_first_line():
     points = [Point(1, 1),
@@ -7,6 +18,72 @@ def test_first_line():
               Point(4, 4),
               Point(5, 5),
               Point(6, 6)]
-    comb_points = list(itertools.combinations(points, 3))
-    manager = Manager(list(comb_points))
+    manager = Manager(points)
     manager.process()
+    assert 1 == manager.count_line()
+
+
+def test_2nd_3rd_lines():
+    points = [Point(7, 1),
+              Point(7, 2),
+              Point(8, 3),
+              Point(8, 4),
+              Point(9, 5),
+              Point(9, 6)]
+    manager = Manager(points)
+    manager.process()
+    assert 2 == manager.count_line()
+
+
+def test_4th_5th_lines():
+    points = [Point(10, 1),
+              Point(10, 2),
+              Point(10, 3),
+              Point(11, 4),
+              Point(11, 5),
+              Point(11, 6)]
+    manager = Manager(points)
+    manager.process()
+    assert 2 == manager.count_line()
+
+
+def test_merge_lines():
+    points = [Point(7, 1),
+              Point(7, 2),
+              Point(8, 3),
+              Point(8, 4),
+              Point(9, 5),
+              Point(9, 6),
+              Point(10, 1),
+              Point(10, 2),
+              Point(10, 3),
+              Point(11, 4),
+              Point(11, 5),
+              Point(11, 6)]
+    manager = Manager(points)
+    manager.process()
+    assert 4 == manager.count_line()
+
+
+def test_least_lines():
+    points = [Point(1, 1),
+              Point(2, 2),
+              Point(3, 3),
+              Point(4, 4),
+              Point(5, 5),
+              Point(6, 6),
+              Point(7, 1),
+              Point(7, 2),
+              Point(8, 3),
+              Point(8, 4),
+              Point(9, 5),
+              Point(9, 6),
+              Point(10, 1),
+              Point(10, 2),
+              Point(10, 3),
+              Point(11, 4),
+              Point(11, 5),
+              Point(11, 6)]
+    manager = Manager(points)
+    manager.process()
+    assert 5 == manager.count_line()
